8a0665019b9b3258d98a19f069cb08282b4727fd
Danielle Kang <daniellekang24@gmail.com>
2020-02-18 10:26:15 -0500

    starting uttc javascript

--- js/ultimatettc-app.js
+++ js/ultimatettc-app.js
@@ -1,27 +1,57 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
-const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+const winningConditionsOverall = [
+  [0, 10, 20],
+  [30, 40, 50],
+  [60, 70, 80],
+  [0, 30, 60],
+  [10, 40, 70],
+  [20, 50, 80],
+  [0, 40, 80],
+  [20, 40, 60]
 ];
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let board;
-
+let turn;
+let win;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 const squares = Array.from(document.querySelectorAll("#board div"));
-const squares1 = Array.from(document.querySelectorAll("#board-1 div"));
-const squares2 = Array.from(document.querySelectorAll("#board-2 div"));
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
-//window.onload = init;
-//document.getElementById("board").onclick = takeTurn;
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
 //document.getElementById("reset-button").onclick = init;
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
+
+function init() {
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+
+  turn = "X";
+  render();
+}
+
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContext = mark;
+  });
+}
+
+function takeTurn(e) {
+  let index = squares.findIndex(function(square) {
+    return square === e.target;
+  });
+
+
+/*
+  board[index] = turn;
+  turn = turn === "X" ? "O" : "X";
+
+  render();
+*/
+}
