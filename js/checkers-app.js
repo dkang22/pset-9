2ff41ade2099a9b7101a87447706349b7ad5bf71
dkang22 <daniellekang24@gmail.com>
2020-02-24 22:42:55 -0500

    checkers @('_')@

--- js/checkers-app.js
+++ js/checkers-app.js
@@ -1,13 +1,33 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
-const board = document.getElementById("board");
+
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
-
+let board;
+let turn;
+let win;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
-
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
+function init() {
+  board = [
+   "", "B", "", "B", "", "B", "", "B",
+   "B", "", "B", "", "B", "", "B", "",
+   "", "B", "", "B", "", "B", "", "B",
+   "", "", "", "", "", "", "", "",
+   "", "", "", "", "", "", "", "",
+   "W", "", "W", "", "W", "", "W", "",
+   "", "W", "", "W", "", "W", "", "W",
+   "W", "", "W", "", "W", "", "W", ""
+ ];
+ turn = "B";
+ win = null;
+// render();
+}
